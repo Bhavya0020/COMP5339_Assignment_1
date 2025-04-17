1fe9bb1d1f9881b3f50fbb94400d2c4184ddcb6c
Sourav Shinde <souravshinde@Souravs-MacBook-Air.local>
2025-04-17 20:58:54 +1000

    Code Cleaned

--- fuelEDA.py
+++ fuelEDA.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
 
-
-df = pd.read_csv("/Users/bhavyadhingra/Desktop/USYD_Study/SEM_3/Data Engineering/COMP5339_Assignment_1/cleaned_fuelcheck_data.csv")
-
+df = pd.read_csv("/cleaned_fuelcheck_data.csv")
 
 print(df.columns)
 
